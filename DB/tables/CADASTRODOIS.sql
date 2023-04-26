2fd12c5abcc98cc5bad2b0e1b81eeb79d2f8b6a9
Mateus Vaz <89218769+mtssvaz@users.noreply.github.com>
2023-04-26 10:32:02 -0300

    código da conexão sem o front

--- DB/tables/CADASTRODOIS.sql
+++ DB/tables/CADASTRODOIS.sql
@@ -1,8 +1,8 @@
 CREATE TABLE CADASTRO
 (
-	
+	ID_CONTRATO INT AUTO_INCREMENT,
     aluno varchar(20),
-	EMAIL varchar(20),
+	Responsavel varchar(20),
     conservacao varchar(20),
     dt_entrega date, 
     MATRICULA int,
