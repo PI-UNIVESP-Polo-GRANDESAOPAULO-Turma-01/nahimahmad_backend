ca519ef6bef19de92a66c2654e9f71ac71844f18
Patrick Rocha <patrick.onix@outlook.com>
2023-04-26 22:19:37 -0300

    Exclusão do e-mail, inclusão do responsável financeiro

--- DB/tables/buscar.php
+++ DB/tables/buscar.php
@@ -5,8 +5,7 @@ include 'conexao.php';
 $buscar_cadastros = " SELECT * FROM  CADASTRO";
 $query_cadastros = mysqli_query($conn, $buscar_cadastros);
 
-    ?>
-
+?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -30,8 +29,8 @@ $query_cadastros = mysqli_query($conn, $buscar_cadastros);
 
 <body>
 
-  <nav class="navbar navbar-expand-md" style="background-color: #324572;">
-    <a class="navbar-brand" href="https://colegioahmad.com.br">
+<nav class="navbar navbar-expand-md" style="background-color: #324572;">
+    <a class="navbar-brand" href="#">
         <img class="logo" src="imagens/logo.png" alt="Logo do colégio Nahim Ahmad">
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
@@ -39,27 +38,20 @@ $query_cadastros = mysqli_query($conn, $buscar_cadastros);
     </button>
 
     <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link link-nav" href="sobre.html">Sobre</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle link-nav" href="cadastro.html" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Cadastro
-          </a>
-          <div class="dropdown-menu">
-            <a class="dropdown-item link-nav-dropdown" href="cadastro.html">Aluno</a>
-            <a class="dropdown-item link-nav-dropdown" href="cadastro.html">Chromebook</a>
-            <a class="dropdown-item link-nav-dropdown" href="cadastro.html">Contrato</a>
-          </div>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link link-nav" href="buscar.html">Buscar</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link link-nav" href="perfil.html">Perfil</a>
-        </li>
-      </ul>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link link-nav" href="#">Início</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link link-nav" href="cadastro.html">Cadastro</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link link-nav" href="buscar.php">Buscar</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link link-nav" href="perfil.html">Perfil</a>
+            </li>
+        </ul>
     </div>
   </nav>
 
@@ -88,19 +80,15 @@ $query_cadastros = mysqli_query($conn, $buscar_cadastros);
     </div>
 
     <div>
-
       <table id="tabela" class="table table-hover">
         <thead>
           <tr>
-            <th>
-              <input class="form-check-input" type="checkbox" id="gridCheck">
-            </th>
-            <th>aluno</th>
-            <th>EMAIL</th>
-            <th>conservacao</th>
-            <th>dt_entrega</th>
-            <th>matricula</th>
-            <th>n_de_serie</th>
+            <th>Aluno</th>
+            <th>Resp. Financeiro</th>
+            <th>Conservacao</th>
+            <th>Data de entrega</th>
+            <th>Matrícula</th>
+            <th>Nº de série</th>
           </tr>
         </thead>
         <tbody>
@@ -110,7 +98,7 @@ $query_cadastros = mysqli_query($conn, $buscar_cadastros);
             {
             
             $receber_cadastros['aluno'];
-            $receber_cadastros ['EMAIL'];
+            $receber_cadastros ['responsavel'];
             $receber_cadastros['conservacao'];
             $receber_cadastros['dt_entrega'];
             $receber_cadastros['matricula'];
@@ -121,13 +109,13 @@ $query_cadastros = mysqli_query($conn, $buscar_cadastros);
           <tr>
               
               <td scope="row"> <?php echo $ $aluno; ?></td>
-              <td>               <?php echo $EMAIL; ?></td>
+              <td>               <?php echo $responsavel; ?></td>
               <td>               <?php echo $conservacao; ?></td>
               <td>               <?php echo $dt_entrega; ?></td>
               <td>               <?php echo $matricula; ?></td>
               <td>               <?php echo $n_de_serie; ?></td>
               
-           </tr>
+          </tr>
           <?php }; ?>
           
           
@@ -140,21 +128,10 @@ $query_cadastros = mysqli_query($conn, $buscar_cadastros);
 
   <footer class="footer">
     <div>
-      <div class="footer_endereco">
-        <p>Avenida Esperança, nº 191 - Centro, Guarulhos/SP <br> Todos os direitos reservados © 2023</p>
-      </div>
-    </div>
-
-    <div class="footer_redes">
-      <div>
-        <p>Nos siga nas redes sociais:</p>
-      </div>
-      <div>
-        <a href="https://www.facebook.com/colegioahmad" class="facebook" target="_blank"><img src="imagens/facebook.png" alt="logo do facebook" width="30px" height="30px"></a>
-        <a href="https://www.instagram.com/colegioahmad/" class="instagram" target="_blank"><img src="imagens/instagram.png" alt="logo do instagram" width="30px" height="30px"></a>
-        <a href="https://twitter.com/colegioahmad" class="twitter" target="_blank"><img src="imagens/twitter.png" alt="logo do twitter" width="30px" height="30px"></a>
-        <a href="https://www.youtube.com/colegioahmad" class="youtube" target="_blank"><img src="imagens/youtube.png" alt="logo do youtube" width="30px" height="30px"></a>   
-      </div>
+        <div>
+            <p>Avenida Esperança, nº 191 - Centro, Guarulhos/SP</p>
+            <p>Todos os direitos reservados © 2023</p>
+        </div>
     </div>
   </footer>
 
